e669215384fd425d7a99e4dd872c28226ccf858b
Michiel Sens <msens@xebia.com>
2013-11-01 14:28:36 +0100

    tekst in NL geplaatst

--- f2c_converter/f2c_convert.php
+++ f2c_converter/f2c_convert.php
@@ -16,7 +16,7 @@
 	<body>
 		<h1>Fahrenheit / Celsius PHP Convertor</h1>
 		<form method="get">
-			Enter number of degrees: <input type="text" name="degrees" /><br />
+			Voeg het aantal graden in: <input type="text" name="degrees" /><br />
 			<select name="conversion">
 				<option value="ctf">Celsius to Fahrenheit</option>
 				<option value="ftc">Fahrenheit to Celsius</option>
